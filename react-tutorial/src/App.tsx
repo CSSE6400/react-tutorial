872a70353fcd8cc8bd0a9cfadd7e595f0fd66114
yourlow <m.holloway@uq.net.au>
2023-05-31 15:51:09 +1000

    removed amplify

--- react-tutorial/src/App.tsx
+++ react-tutorial/src/App.tsx
@@ -4,35 +4,8 @@ import Create from './Todo/Create';
 import View from './Todo/View';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-import { Amplify } from 'aws-amplify';
-
-import { withAuthenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
-
-
-
-Amplify.configure({
-  Auth: {
-
-      // REQUIRED - Amazon Cognito Region
-      region: 'region',
-
-      // OPTIONAL - Amazon Cognito User Pool ID
-      userPoolId: 'user-pool-id',
-
-      // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
-      userPoolWebClientId: 'client-id',
-
-      // OPTIONAL - Enforce user authentication prior to accessing AWS resources or not
-      mandatorySignIn: true,
-
-  }
-});
-
-
-function App(props?: { signOut: any; user: any }) {  
-  console.log(props?.user)
+function App() {
+  console.log(props?.user);
   return (
     <div>
       <Routes>
@@ -45,4 +18,4 @@ function App(props?: { signOut: any; user: any }) {
 }
 
 // eslint-disable-next-line react-refresh/only-export-components
-export default withAuthenticator(App);
+export default App;
